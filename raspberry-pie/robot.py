3b084d665b4757f64b7debd41e85ca088b497029
EgorHivrich <111039695+EgorHivrich@users.noreply.github.com>
2024-04-23 18:50:31 +1000

    Update robot.py

--- raspberry-pie/robot.py
+++ raspberry-pie/robot.py
@@ -5,19 +5,19 @@ import functions
 
 class CreatingRobotException(Exception):
     def __init__(self) -> None:
-	self.message = "Creating robot exception"
+		self.message = "Creating robot exception"
 
     def __str__(self) -> str:
-	return self.message
+		return self.message
 
 class Robot:
     def __init__(self, deserializer : Deserializer) -> None:
-	self.commands = deserializer.decode()
-	if commands == []:
-		raise CreatingCommandException()
-	print(self.commands)
+		self.commands = deserializer.decode()
+		if commands == []:
+			raise CreatingCommandException()
+		print(self.commands)
 		
     def __getitem__(self, key : str) -> None:
-	for command in self.commands:
-	    result = command if key == command.text else None
-	functions.speak(result)
+		for command in self.commands:
+	    	result = command if key == command.text else None
+		functions.speak(result)
