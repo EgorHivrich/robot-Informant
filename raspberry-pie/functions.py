c3f48848a09fe8059484889e4d0c65e2381d46ed
Egor <egorhacker05@gmail.com>
2024-04-27 23:27:07 +1000

    Update 27.04.2024 Uploaded by Egor Hivrivh date 23:27

--- raspberry-pie/functions.py
+++ raspberry-pie/functions.py
@@ -3,9 +3,8 @@ import pyttsx3, speech_recognition, asyncio, time
 def convert_speech_to_text() -> str:
 	result, recognizer = "", speech_recognition.Recognizer()
 	with speech_recognition.Microphone() as source:
-		recognizer.adjust_for_ambient_noise(source, duration = 0.2)
-		result = recognizer.recognize_google(recognizer.listen())
-	return result.lower()
+		result = recognizer.listen(source)
+	return recognizer.recognize_google(result, language = "ru-RU").lower()
 
 def speak(message : str) -> None:
 	driver = pyttsx3.init()
--- raspberry-pie/robot_informant.py
+++ raspberry-pie/robot_informant.py
@@ -3,14 +3,19 @@ import json
 
 class RobotInformant:
 	def __init__(self, configPath : str) -> None:
-		with open(configPath, encoding = "UTF-8") as file:
-			self.__dict__.update(**json.loads(file.read()))
+		self._deserialize(configPath)
 		if self.commands == []: raise CreatingRobotException()
 		
 	def __getitem__(self, key : str) -> str:
+		result = None
 		for command in self.commands:
 			result = command if key == command.text else None
 		return result
+
+	def _deserialize(self, configPath : str) -> None:
+		with open(configPath, encoding = "UTF-8") as file:
+			self.__dict__.update(**json.loads(file.read()))
+		self.commands = list(map(lambda arg: Command(**arg), self.commands))
 
 class CreatingRobotException(BaseException):
 	def __init__(self) -> None:
